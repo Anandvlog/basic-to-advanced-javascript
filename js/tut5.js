9f4e74b3837cd33a0beff389abe581a3ca559c8d
Anand Chauhan <71122607+Anandvlog@users.noreply.github.com>
2021-04-24 13:25:44 +0530

    Delete js directory

--- js/tut5.js
+++
@@ -1,9 +0,0 @@
-let name = "anand";
-let fruits1 ="apple";
-let fruits2 = 'orange';
-let myhtml = `hello ${name} 
-            <h1>This is heading</h1>
-            <p> You like ${fruits1} and ${fruits2} 
-
-`;
-document.body.innerHTML = myhtml;
